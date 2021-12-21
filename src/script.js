fa9c716c067704cbf4e26cdf3d12e2d3b451bc8f
Christopher Fanning <chris@christopherfanning.com>
2021-12-21 17:34:27 -0600

    Current player status indicated

--- src/script.js
+++ src/script.js
@@ -4,16 +4,20 @@ let currentPlayer = 0;
 // let p1 = "orange";
 // let p2 = "green";
 let color = "orange";
+const playerColor = document.querySelector('.color');
 
 function togglePlayer() {
   if (currentPlayer === 0) {
     currentPlayer = 1;
     color = "green";
+    playerColor.style.backgroundColor = "green";
   } else {
     currentPlayer = 0;
     color = "orange";
+    playerColor.style.backgroundColor = "orange";
   }
 }
+
 
 
 cells.forEach((cell, index) => {
