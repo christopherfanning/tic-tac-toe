2d6b932d5776b45e71765c2b8c5c68848c555a9c
Christopher Fanning <chris@christopherfanning.com>
2021-12-21 17:29:23 -0600

    Add some styles, alternate player colors

--- src/script.js
+++ src/script.js
@@ -1,7 +1,25 @@
 let cells = document.querySelectorAll('.cell');
 
+let currentPlayer = 0;
+// let p1 = "orange";
+// let p2 = "green";
+let color = "orange";
+
+function togglePlayer() {
+  if (currentPlayer === 0) {
+    currentPlayer = 1;
+    color = "green";
+  } else {
+    currentPlayer = 0;
+    color = "orange";
+  }
+}
+
+
 cells.forEach((cell, index) => {
   cell.addEventListener('click', () => {
     console.log("you clicked" + index);
+    cell.style.backgroundColor = color;
+    togglePlayer();
   });
 });
