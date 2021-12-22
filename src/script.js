8409d623abc8f3ef897445f890980671bff2d30f
Christopher Fanning <chris@christopherfanning.com>
2021-12-22 11:04:25 -0600

    Add all winning conditions

--- src/script.js
+++ src/script.js
@@ -37,7 +37,14 @@ function checkWinningCondition() {
   // Check the array for possible winning conditions. 
   let winningConditions = [
 
-    [0, ]
+    [0, 3, 6],
+    [1, 4, 8],
+    [2, 5, 8],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6]
   ];
 
   // or use an object instead of an array.
