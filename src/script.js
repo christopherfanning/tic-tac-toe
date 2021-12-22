395f55a702a3e98097839e83ce435b9a78df95b5
Christopher Fanning <chris@christopherfanning.com>
2021-12-22 17:22:02 -0600

    Win conditions realized, slight refactor.

--- src/script.js
+++ src/script.js
@@ -5,8 +5,6 @@ let moves = [
 ];
 
 let currentPlayer = 0;
-// let p1 = "orange";
-// let p2 = "green";
 let color = "orange";
 const playerColor = document.querySelector('.color');
 
@@ -22,20 +20,18 @@ function togglePlayer() {
   }
 }
 
-function handleClick(cell, index) {
-  moves[currentPlayer].push(index);
+function handleClick(cell, cellClicked) {
+  moves[currentPlayer].push(cellClicked);
   cell.style.backgroundColor = color;
   checkWinningCondition();
   togglePlayer();
   cell.style.pointerEvents = "none";
-  console.log(moves);
 }
 
 
 function checkWinningCondition() {
   // Check the movesArray for current player for possible winning conditions. 
   let winningConditions = [
-
     [0, 3, 6],
     [1, 4, 8],
     [2, 5, 8],
@@ -46,44 +42,23 @@ function checkWinningCondition() {
     [2, 4, 6]
   ];
 
-  // let checker = (arr, target) => target.every(v => arr.includes(v));
-  let checker = moves[currentPlayer].every(v => {
-    return winningConditions.forEach(arr => {
-      return arr.includes(v);
-      // console.log(`player ${currentPlayer} wins!!`);
+  function isWinPresent(moves, winCondition) {
+    return winCondition.every(vals => {
+      return moves.includes(vals);
     });
-  });
-
-  let cwc = winningConditions.forEach(condition => {
-
-    return condition.every(arr => {
-      return moves[currentPlayer].includes(arr);
-    });
-  });
+  }
 
   winningConditions.forEach(arr => {
-    if (moves[currentPlayer].includes(arr.values())) {
-      console.log("yay!");
+    if (isWinPresent(moves[currentPlayer], arr)) {
+      console.log(`!!!!!!!!!!!!!!!!!!!Player ${currentPlayer} wins!!!!!!!!!!!!!!!!!!!!!!!!!`);
+      // TODO alert on page, disable anymore clicks. maybe use an overlay. 
     }
-
   });
-  console.log(checker);
-  console.log(cwc);
-
-  if (checker == true) {
-
-    console.log(`player ${currentPlayer} wins!!`);
-  }
-  // moves[currentPlayer].forEach(move => {
-  //   console.log(move);
-
-  // });
 }
 
 
-
-cells.forEach((cell, index) => {
+cells.forEach((cell, cellNumber) => {
   cell.addEventListener('click', () => {
-    handleClick(cell, index);
+    handleClick(cell, cellNumber);
   });
 });
